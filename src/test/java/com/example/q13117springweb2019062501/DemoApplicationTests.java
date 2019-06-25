9b5d0164258c297b76f578594a4efc6e8f8dd51a
q13117 <carlxie@hotmail.com>
2019-06-25 14:40:26 +0000

    Initial import

---
+++ src/test/java/com/example/q13117springweb2019062501/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.q13117springweb2019062501;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
